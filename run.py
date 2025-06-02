271ff9697fb74221fd1af62888d6011aaca947fe
xmozzi <mozzillc@gmail.com>
2025-06-02 21:20:22 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,6 @@
-from flask import Flask, jsonify
+from app import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, Flask! Welcome to your first API."
-
-@app.route('/api', methods=['GET'])
-def api():
-    data = {
-        "message": "Welcome to your Flask API!",
-        "status": "success"
-    }
-    return jsonify(data)
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
