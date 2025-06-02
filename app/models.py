1067d62f38cfb5f370cb1e4d9198e5813eca7b14
xmozzi <mozzillc@gmail.com>
2025-06-02 21:19:58 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,20 +1,13 @@
-from . import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+db = SQLAlchemy()
+ma = Marshmallow()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.username}>'
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    user = db.relationship('User', backref='posts', lazy=True)
-
-    def __repr__(self):
-        return f'<Post {self.title}>'
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
