dd51f1604506caf71c0991dfe3269723e3eade63
xmozzi <mozzillc@gmail.com>
2025-06-02 21:19:47 +0700

    Update errors.py

--- app/errors.py
+++ app/errors.py
@@ -1,10 +1,4 @@
 from flask import jsonify
-from . import app
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return jsonify({"error": "Not Found", "message": "The requested URL was not found on the server."}), 404
-
-@app.errorhandler(500)
-def internal_error(error):
-    return jsonify({"error": "Internal Server Error", "message": "Something went wrong. Please try again later."}), 500
+def handle_error(e):
+    return jsonify(error=str(e)), 500
